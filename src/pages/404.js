30606d0528d66acf9fe04c9a51b199e8674de752
Justin Spegele <justin.spegele@gmail.com>
2020-09-23 17:00:39 -0400

    Updated readme

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,13 +3,31 @@ import { Link } from 'gatsby'
 
 import Head from '../components/head'
 import Layout from '../components/layout'
+import Container from '../components/container'
 
 const NotFound = () => {
   return (
     <Layout>
       <Head title="404" />
-      <h1>Page not found</h1>
-      <p><Link to="/">Head home</Link></p>
+      <Container>
+        <div style={{ 
+          color: 'white',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: 15+'rem',
+          maxWidth: 30+'rem',
+          textAlign: 'center'
+        }}>
+          <h1>Page not found</h1>
+          <p style={{ marginTop: 2+'rem' }}>
+            <Link to="/" style={{
+              color: '#688ddf',
+              fontSize: 2+'rem',
+              textDecoration: 'none'
+            }}>Head home</Link>
+            </p>
+        </div>
+      </Container>
     </Layout>
   )
 }
