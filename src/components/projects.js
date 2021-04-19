664b936f0d255089691845e2c9143e74d2361940
Justin Spegele <justin.spegele@gmail.com>
2021-04-19 10:06:37 -0400

    Redesigned work and projects, added pfs, optimized images

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { FaPenSquare, FaBitcoin, FaPiggyBank, FaGamepad, FaRegCommentDots, FaExternalLinkAlt } from 'react-icons/fa'
+import React from "react"
+import {
+  FaPenSquare,
+  FaBitcoin,
+  FaPiggyBank,
+  FaGamepad,
+  FaRegCommentDots,
+  FaExternalLinkAlt,
+} from "react-icons/fa"
 
-import Container from './container'
+import Container from "./container"
 
-import chenelSecuritiesThumb from '../images/chenel-securities-sm.png'
-import crwnClowthingThumb from '../images/crwn-clothing-sm.png'
-import styles from './styles/projects.module.scss'
-import buttonStyles from './styles/buttons.module.scss'
+import pfsThumb from "../images/parasky-flute-studios-sm.jpg"
+import chenelSecuritiesThumb from "../images/chenel-securities-sm.jpg"
+import crwnClowthingThumb from "../images/crwn-clothing-sm.jpg"
+import styles from "./styles/projects.module.scss"
+import buttonStyles from "./styles/buttons.module.scss"
 
 const Projects = () => (
   <div className={styles.projectsWrapper} id="projects">
@@ -16,36 +24,62 @@ const Projects = () => (
         <div className={styles.hr}></div>
       </div>
       <section className={styles.works}>
-        <a
-          className={styles.work}
-          style={{ backgroundImage: "url(" + chenelSecuritiesThumb + ")" }}
-          href="https://chenelsecurities.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className={styles.workOverlay}>
-            <span className={styles.workName}>Chenel Securities</span>
-            <span className={styles.workDesc}>A Manhattan-based private investment firm</span>
-            <button className={styles.siteButton}>
+        <div className={styles.work}>
+          <img src={chenelSecuritiesThumb} alt="Chenel Capital screenshot" />
+          <div className={styles.workContent}>
+            <h3>Chenel Capital</h3>
+            <p className={styles.workDesc}>
+              chenelcapital.com showcases and innovative, Manhattan-based
+              private equity firm that specializing in growth, venture, and
+              angel stage investments.
+            </p>
+            <a
+              className={styles.siteButton}
+              href="https://chenelcapital.com"
+              target="_blank"
+              rel="noreferrer"
+            >
               Visit Site <FaExternalLinkAlt />
-            </button>
+            </a>
           </div>
-        </a>
-        <a
-          className={styles.work}
-          style={{ backgroundImage: "url(" + crwnClowthingThumb + ")" }}
-          href="https://crwn.justinspegele.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className={styles.workOverlay}>
-            <span className={styles.workName}>Crwn Clothing</span>
-            <span className={styles.workDesc}>A fully functional, React-based ecommerce shop.</span>
-            <button className={styles.siteButton}>
+        </div>
+        <div className={styles.work}>
+          <img src={pfsThumb} alt="Parasky Flute Studios screenshot" />
+          <div className={styles.workContent}>
+            <h3>Parasky Flute Studios</h3>
+            <p className={styles.workDesc}>
+              paraskyflutestudios.com is a modern small business website
+              designed to drive conversions of new online business for
+              the studio in a virtual world.
+            </p>
+            <a
+              className={styles.siteButton}
+              href="https://paraskyflutestudios.com"
+              target="_blank"
+              rel="noreferrer"
+            >
               Visit Site <FaExternalLinkAlt />
-            </button>
+            </a>
           </div>
-        </a>
+        </div>
+        <div className={styles.work}>
+          <img src={crwnClowthingThumb} alt="Crwn Clothing screenshot" />
+          <div className={styles.workContent}>
+            <h3>Crwn Clothing</h3>
+            <p className={styles.workDesc}>
+              A fully functional, React-based ecommerce shop with a modern and
+              responsive design to showcase your products.
+            </p>
+            <a
+              className={styles.siteButton}
+              href="https://crwn.justinspegele.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit Site <FaExternalLinkAlt />
+            </a>
+          </div>
+        </div>
       </section>
       <section className={styles.projects}>
         <div className={styles.project}>
@@ -55,7 +89,10 @@ const Projects = () => (
             rel="noreferrer"
             className={styles.info}
           >
-            <h4><FaPenSquare />Notebook</h4>
+            <h4>
+              <FaPenSquare />
+              Notebook
+            </h4>
             <span>A free and open-source note-taking app for the web.</span>
           </a>
           <div className={styles.source}>
@@ -76,8 +113,14 @@ const Projects = () => (
             rel="noreferrer"
             className={styles.info}
           >
-            <h4><FaBitcoin />CryptoDash</h4>
-            <span>Crypto currency dashboard to track coins and look up prices and historical data.</span>
+            <h4>
+              <FaBitcoin />
+              CryptoDash
+            </h4>
+            <span>
+              Crypto currency dashboard to track coins and look up prices and
+              historical data.
+            </span>
           </a>
           <div className={styles.source}>
             <a
@@ -97,8 +140,13 @@ const Projects = () => (
             rel="noreferrer"
             className={styles.info}
           >
-            <h4><FaPiggyBank />Expensify</h4>
-            <span>A simple and intuitive budgeting and expense management app.</span>
+            <h4>
+              <FaPiggyBank />
+              Expensify
+            </h4>
+            <span>
+              A simple and intuitive budgeting and expense management app.
+            </span>
           </a>
           <div className={styles.source}>
             <a
@@ -118,8 +166,14 @@ const Projects = () => (
             rel="noreferrer"
             className={styles.info}
           >
-            <h4><FaGamepad />Warframe Checklist</h4>
-            <span>A tool for Warframe players to keep track of gear they've acquired and mastered, and what they still need.</span>
+            <h4>
+              <FaGamepad />
+              Warframe Checklist
+            </h4>
+            <span>
+              A tool for Warframe players to keep track of gear they've acquired
+              and mastered, and what they still need.
+            </span>
           </a>
           <div className={styles.source}>
             <a
@@ -160,8 +214,14 @@ const Projects = () => (
             rel="noreferrer"
             className={styles.info}
           >
-            <h4><FaRegCommentDots />DevChat</h4>
-            <span>A slack clone for development teams to collaborate via real-time chat.</span>
+            <h4>
+              <FaRegCommentDots />
+              DevChat
+            </h4>
+            <span>
+              A slack clone for development teams to collaborate via real-time
+              chat.
+            </span>
           </a>
           <div className={styles.source}>
             <a
