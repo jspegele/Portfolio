6c62e793a51a4a09ef39a3343d8d83018abc0b37
Justin Spegele <justin.spegele@gmail.com>
2021-04-30 15:21:17 -0400

    Added modals for recent work with extended description and screenshots in carousel

---
+++ src/components/project-modal.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FaExternalLinkAlt } from "react-icons/fa"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel'
+
+import styles from './styles/project-modal.module.scss'
+
+const ProjectModal = ({ isActive, handleCloseModal, title, images, description, link }) => (
+  <div className={`${styles.modal} ${isActive ? styles.isActive : ''}`}>
+    <div className={styles.modalBackground}></div>
+    <div className={styles.modalContent}>
+      <header className={styles.modalContentHead}>
+        <p className={styles.modalContentTitle}>
+          {title}
+        </p>
+        <button className={styles.modalClose} onClick={handleCloseModal}></button>
+      </header>
+      <div className={styles.modalContentImage}>
+        {images.length && (
+          <Carousel showThumbs={false}>
+            {images.map(image => (
+              <div>
+                <img src={image.url} alt="Website screenshot" />
+              </div>
+            ))}
+          </Carousel>
+        )}
+      </div>
+      <footer className={styles.modalContentFoot}>
+        {description}
+        <a
+          href={link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Visit {title} <FaExternalLinkAlt />
+        </a>
+      </footer>
+    </div>
+  </div>
+)
+ 
+export default ProjectModal
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import {
   FaPenSquare,
   FaBitcoin,
@@ -9,238 +9,310 @@ import {
 } from "react-icons/fa"
 
 import Container from "./container"
+import ProjectModal from "./project-modal"
 
 import pfsThumb from "../images/parasky-flute-studios-sm.jpg"
+import pfsScreen1 from "../images/parasky-flute-studios-screen1.jpg"
+import pfsScreen2 from "../images/parasky-flute-studios-screen2.jpg"
+import pfsScreen3 from "../images/parasky-flute-studios-screen3.jpg"
+import pfsScreen4 from "../images/parasky-flute-studios-screen4.jpg"
+import pfsScreen5 from "../images/parasky-flute-studios-screen5.jpg"
+import pfsScreen6 from "../images/parasky-flute-studios-screen6.jpg"
 import chenelSecuritiesThumb from "../images/chenel-securities-sm.jpg"
-import crwnClowthingThumb from "../images/crwn-clothing-sm.jpg"
+import chenelScreen1 from "../images/chenel-capital-screen1.jpg"
+import chenelScreen2 from "../images/chenel-capital-screen2.jpg"
+import chenelScreen3 from "../images/chenel-capital-screen3.jpg"
+import chenelScreen4 from "../images/chenel-capital-screen4.jpg"
+import chenelScreen5 from "../images/chenel-capital-screen5.jpg"
+import crwnClothingThumb from "../images/crwn-clothing-sm.jpg"
+import crwnClothingLarge from "../images/crwn-clothing-lg.jpg"
 import styles from "./styles/projects.module.scss"
 import buttonStyles from "./styles/buttons.module.scss"
 
-const Projects = () => (
-  <div className={styles.projectsWrapper}>
-    <Container>
-      <div className={styles.header}>
-        <h2 id="projects">Recent Work</h2>
-        <div className={styles.hr}></div>
-      </div>
-      <section className={styles.works}>
-        <div className={styles.work}>
-          <img src={chenelSecuritiesThumb} alt="Chenel Capital screenshot" />
-          <div className={styles.workContent}>
-            <h3>Chenel Capital</h3>
-            <p className={styles.workDesc}>
-              chenelcapital.com showcases and innovative, Manhattan-based
-              private equity firm that specializing in growth, venture, and
-              angel stage investments.
-            </p>
-            <a
-              className={styles.siteButton}
-              href="https://chenelcapital.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Visit Site <FaExternalLinkAlt />
-            </a>
-          </div>
+const Projects = () => {
+  const [modal1Open, setModal1Open] = useState(false)
+  const [modal2Open, setModal2Open] = useState(false)
+  const [modal3Open, setModal3Open] = useState(false)
+
+  return (
+    <div className={styles.projectsWrapper}>
+      <Container>
+        <div className={styles.header}>
+          <h2 id="projects">Recent Work</h2>
+          <div className={styles.hr}></div>
         </div>
-        <div className={styles.work}>
-          <img src={pfsThumb} alt="Parasky Flute Studios screenshot" />
-          <div className={styles.workContent}>
-            <h3>Parasky Flute Studios</h3>
-            <p className={styles.workDesc}>
-              paraskyflutestudios.com is a modern small business website
-              designed to drive conversions of new online business for
-              the studio in a virtual world.
-            </p>
-            <a
-              className={styles.siteButton}
-              href="https://paraskyflutestudios.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Visit Site <FaExternalLinkAlt />
-            </a>
+        <section className={styles.works}>
+          <div className={styles.work} onClick={() => setModal2Open(true)}>
+            <img src={pfsThumb} alt="Parasky Flute Studios screenshot" />
+            <div className={styles.workContent}>
+              <h3>Parasky Flute Studios</h3>
+              <p className={styles.workDesc}>
+                paraskyflutestudios.com is a modern small business website
+                designed to drive conversions of new online business for the
+                studio in a virtual world.
+              </p>
+            </div>
           </div>
+          <ProjectModal
+            isActive={modal2Open}
+            handleCloseModal={() => setModal2Open(false)}
+            title="Parasky Flute Studios"
+            images={[
+              { url: pfsScreen1 },
+              { url: pfsScreen2 },
+              { url: pfsScreen3 },
+              { url: pfsScreen4 },
+              { url: pfsScreen5 },
+              { url: pfsScreen6 },
+            ]}
+            description={
+              <>
+                <p>
+                  Parasky Flute Studios is a private music studio offering music 
+                  lessons in the greater Pittsburgh area as well as virtually to 
+                  students around the world. They approched me to help them rebuild
+                  their online presence, improve SEO, and increase conversions of
+                  new online students.
+                </p>
+                <p>
+                  Built with GatsbyJS and a Contentful integration, paraskyflutestudios.com 
+                  is a fast, dynamic,responsive and easy to maintain website designed 
+                  to drive sales through an exceptional user experience.
+                </p>
+              </>
+            }
+            link="https://paraskyflutestudios.com"
+          />
+          <div className={styles.work} onClick={() => setModal1Open(true)}>
+            <img src={chenelSecuritiesThumb} alt="Chenel Capital screenshot" />
+            <div className={styles.workContent}>
+              <h3>Chenel Capital</h3>
+              <p className={styles.workDesc}>
+                Chenel Capital is an innovative, Manhattan-based private equity
+                firm that specializing in growth, venture, and angel stage
+                investments.
+              </p>
+            </div>
+          </div>
+          <ProjectModal
+            isActive={modal1Open}
+            handleCloseModal={() => setModal1Open(false)}
+            title="Chenel Capital"
+            images={[
+              { url: chenelScreen1 },
+              { url: chenelScreen2 },
+              { url: chenelScreen3 },
+              { url: chenelScreen4 },
+              { url: chenelScreen5 },
+            ]}
+            description={
+              <>
+                <p>
+                  Chenel Capital is an innovative, Manhattan-based private
+                  equity firm that specializing in growth, venture, and angel
+                  stage investments. They approached me to build a clean,
+                  professional, and minimalistic website with them that
+                  showcases their phenomenal team and modern appraoch in the
+                  industry.
+                </p>
+                <p>
+                  Built with GatsbyJS, chenelcapital.com is a fast, dynamic,
+                  responsive and easy to maintain website with a powerful news
+                  publishing feature.
+                </p>
+              </>
+            }
+            link="https://chenelcapital.com"
+          />
+          <div className={styles.work} onClick={() => setModal3Open(true)}>
+            <img src={crwnClothingThumb} alt="Crwn Clothing screenshot" />
+            <div className={styles.workContent}>
+              <h3>Crwn Clothing</h3>
+              <p className={styles.workDesc}>
+                A fully functional, React-based ecommerce shop with a modern and
+                responsive design to showcase your products.
+              </p>
+            </div>
+          </div>
+          <ProjectModal
+            isActive={modal3Open}
+            handleCloseModal={() => setModal3Open(false)}
+            title="Crwn Clothing"
+            images={[
+              { url: crwnClothingLarge },
+            ]}
+            description={
+              <>
+                <p>
+                  A fully functional, React-based ecommerce shop with a modern and
+                  responsive design to showcase your products.
+                </p>
+              </>
+            }
+            link="https://crwn-books.herokuapp.com/"
+          />
+        </section>
+        <div className={styles.header} style={{ marginTop: "80px" }}>
+          <h2>Misc Projects</h2>
+          <div className={styles.hr}></div>
         </div>
-        <div className={styles.work}>
-          <img src={crwnClowthingThumb} alt="Crwn Clothing screenshot" />
-          <div className={styles.workContent}>
-            <h3>Crwn Clothing</h3>
-            <p className={styles.workDesc}>
-              A fully functional, React-based ecommerce shop with a modern and
-              responsive design to showcase your products.
-            </p>
+        <section className={styles.projects}>
+          <div className={styles.project}>
             <a
-              className={styles.siteButton}
-              href="https://crwn-books.herokuapp.com/"
+              href="https://notebook.justinspegele.com"
               target="_blank"
               rel="noreferrer"
+              className={styles.info}
             >
-              Visit Site <FaExternalLinkAlt />
+              <h4>
+                <FaPenSquare />
+                Notebook
+              </h4>
+              <span>A free and open-source note-taking app for the web.</span>
             </a>
+            <div className={styles.source}>
+              <a
+                className={buttonStyles.buttonSecondary}
+                href="https://github.com/jspegele/Notebook"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code
+              </a>
+            </div>
           </div>
-        </div>
-      </section>
-      <div className={styles.header} style={{ marginTop: '80px' }}>
-        <h2>Misc Projects</h2>
-        <div className={styles.hr}></div>
-      </div>
-      <section className={styles.projects}>
-        <div className={styles.project}>
-          <a
-            href="https://notebook.justinspegele.com"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.info}
-          >
-            <h4>
-              <FaPenSquare />
-              Notebook
-            </h4>
-            <span>A free and open-source note-taking app for the web.</span>
-          </a>
-          <div className={styles.source}>
+          <div className={styles.project}>
             <a
-              className={buttonStyles.buttonSecondary}
-              href="https://github.com/jspegele/Notebook"
+              href="http://crypto.justinspegele.com/"
               target="_blank"
               rel="noreferrer"
+              className={styles.info}
             >
-              Source Code
+              <h4>
+                <FaBitcoin />
+                CryptoDash
+              </h4>
+              <span>
+                Crypto currency dashboard to track coins and look up prices and
+                historical data.
+              </span>
             </a>
+            <div className={styles.source}>
+              <a
+                className={buttonStyles.buttonSecondary}
+                href="https://github.com/jspegele/cryptodash"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code
+              </a>
+            </div>
           </div>
-        </div>
-        <div className={styles.project}>
-          <a
-            href="http://crypto.justinspegele.com/"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.info}
-          >
-            <h4>
-              <FaBitcoin />
-              CryptoDash
-            </h4>
-            <span>
-              Crypto currency dashboard to track coins and look up prices and
-              historical data.
-            </span>
-          </a>
-          <div className={styles.source}>
+          <div className={styles.project}>
             <a
-              className={buttonStyles.buttonSecondary}
-              href="https://github.com/jspegele/cryptodash"
+              href="http://expensify.justinspegele.com/"
               target="_blank"
               rel="noreferrer"
+              className={styles.info}
             >
-              Source Code
+              <h4>
+                <FaPiggyBank />
+                Expensify
+              </h4>
+              <span>
+                A simple and intuitive budgeting and expense management app.
+              </span>
             </a>
+            <div className={styles.source}>
+              <a
+                className={buttonStyles.buttonSecondary}
+                href="https://github.com/jspegele/expensify-app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code
+              </a>
+            </div>
           </div>
-        </div>
-        <div className={styles.project}>
-          <a
-            href="http://expensify.justinspegele.com/"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.info}
-          >
-            <h4>
-              <FaPiggyBank />
-              Expensify
-            </h4>
-            <span>
-              A simple and intuitive budgeting and expense management app.
-            </span>
-          </a>
-          <div className={styles.source}>
+          <div className={styles.project}>
             <a
-              className={buttonStyles.buttonSecondary}
-              href="https://github.com/jspegele/expensify-app"
+              href="https://warframe-checklist.herokuapp.com/"
               target="_blank"
               rel="noreferrer"
+              className={styles.info}
             >
-              Source Code
+              <h4>
+                <FaGamepad />
+                Warframe Checklist
+              </h4>
+              <span>
+                A tool for Warframe players to keep track of gear they've
+                acquired and mastered, and what they still need.
+              </span>
             </a>
+            <div className={styles.source}>
+              <a
+                className={buttonStyles.buttonSecondary}
+                href="https://github.com/jspegele/warframe-checklist"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code
+              </a>
+            </div>
           </div>
-        </div>
-        <div className={styles.project}>
-          <a
-            href="https://warframe-checklist.herokuapp.com/"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.info}
-          >
-            <h4>
-              <FaGamepad />
-              Warframe Checklist
-            </h4>
-            <span>
-              A tool for Warframe players to keep track of gear they've acquired
-              and mastered, and what they still need.
-            </span>
-          </a>
-          <div className={styles.source}>
+          {/* <div className={styles.project}>
             <a
-              className={buttonStyles.buttonSecondary}
-              href="https://github.com/jspegele/warframe-checklist"
+              href="https://tny-link.herokuapp.com/"
               target="_blank"
               rel="noreferrer"
+              className={styles.info}
             >
-              Source Code
+              <h4><FaLink />TnyLnk</h4>
+              <span>Link shortner that generates easy, readable, tiny links.</span>
             </a>
-          </div>
-        </div>
-        {/* <div className={styles.project}>
-          <a
-            href="https://tny-link.herokuapp.com/"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.info}
-          >
-            <h4><FaLink />TnyLnk</h4>
-            <span>Link shortner that generates easy, readable, tiny links.</span>
-          </a>
-          <div className={styles.source}>
+            <div className={styles.source}>
+              <a
+                className={buttonStyles.buttonSecondary}
+                href="https://github.com/jspegele/tny-lnk"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code
+              </a>
+            </div>
+          </div> */}
+          <div className={styles.project}>
             <a
-              className={buttonStyles.buttonSecondary}
-              href="https://github.com/jspegele/tny-lnk"
+              href="http://devchat.justinspegele.com/"
               target="_blank"
               rel="noreferrer"
+              className={styles.info}
             >
-              Source Code
+              <h4>
+                <FaRegCommentDots />
+                DevChat
+              </h4>
+              <span>
+                A slack clone for development teams to collaborate via real-time
+                chat.
+              </span>
             </a>
+            <div className={styles.source}>
+              <a
+                className={buttonStyles.buttonSecondary}
+                href="https://github.com/jspegele/Devchat"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code
+              </a>
+            </div>
           </div>
-        </div> */}
-        <div className={styles.project}>
-          <a
-            href="http://devchat.justinspegele.com/"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.info}
-          >
-            <h4>
-              <FaRegCommentDots />
-              DevChat
-            </h4>
-            <span>
-              A slack clone for development teams to collaborate via real-time
-              chat.
-            </span>
-          </a>
-          <div className={styles.source}>
-            <a
-              className={buttonStyles.buttonSecondary}
-              href="https://github.com/jspegele/Devchat"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Source Code
-            </a>
-          </div>
-        </div>
-      </section>
-    </Container>
-  </div>
-)
+        </section>
+      </Container>
+    </div>
+  )
+}
 
 export default Projects
