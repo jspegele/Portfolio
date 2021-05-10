5c8368d5031fbfcce3f39e802d0b0922920a43e4
Justin Spegele <justin.spegele@gmail.com>
2021-05-09 20:09:50 -0400

    Fixed zindex of modal over header

--- src/components/project-modal.js
+++ src/components/project-modal.js
@@ -5,39 +5,47 @@ import { Carousel } from 'react-responsive-carousel'
 
 import styles from './styles/project-modal.module.scss'
 
-const ProjectModal = ({ isActive, handleCloseModal, title, images, description, link }) => (
-  <div className={`${styles.modal} ${isActive ? styles.isActive : ''}`}>
-    <div className={styles.modalBackground}></div>
-    <div className={styles.modalContent}>
-      <header className={styles.modalContentHead}>
-        <p className={styles.modalContentTitle}>
-          {title}
-        </p>
-        <button className={styles.modalClose} onClick={handleCloseModal} aria-label="Close"></button>
-      </header>
-      <div className={styles.modalContentImage}>
-        {images.length && (
-          <Carousel showThumbs={false}>
-            {images.map(image => (
-              <div>
-                <img src={image.url} alt="Website screenshot" />
-              </div>
-            ))}
-          </Carousel>
-        )}
+const ProjectModal = ({ isActive, handleCloseModal, title, images, description, link }) => {
+  const handleKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      handleCloseModal()
+    }
+  }
+
+  return (
+    <div className={`${styles.modal} ${isActive ? styles.isActive : ''}`}>
+      <div className={styles.modalBackground} onClick={handleCloseModal} onKeyDown={handleKeyDown} role="presentation"></div>
+      <div className={styles.modalContent}>
+        <header className={styles.modalContentHead}>
+          <p className={styles.modalContentTitle}>
+            {title}
+          </p>
+          <button className={styles.modalClose} onClick={handleCloseModal} aria-label="Close"></button>
+        </header>
+        <div className={styles.modalContentImage}>
+          {images.length && (
+            <Carousel showThumbs={false}>
+              {images.map(image => (
+                <div>
+                  <img src={image.url} alt="Website screenshot" />
+                </div>
+              ))}
+            </Carousel>
+          )}
+        </div>
+        <footer className={styles.modalContentFoot}>
+          {description}
+          <a
+            href={link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Visit {title} <FaExternalLinkAlt />
+          </a>
+        </footer>
       </div>
-      <footer className={styles.modalContentFoot}>
-        {description}
-        <a
-          href={link}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Visit {title} <FaExternalLinkAlt />
-        </a>
-      </footer>
     </div>
-  </div>
-)
+  )
+}
  
 export default ProjectModal
